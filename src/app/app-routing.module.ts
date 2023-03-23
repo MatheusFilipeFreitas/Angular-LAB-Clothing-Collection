ec683766735c9e0e8c97164264f04e90e11a0d66
MatheusFreitas <mathffreitas@hotmail.com>
2023-03-23 08:58:47 -0300

    Criação dos campos da Tela de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullComponent } from './layouts/full/full.component';
+import { WrapperComponent } from './layouts/wrapper/wrapper.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FullComponent
+    component: WrapperComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      }
+    ]
   }
 ];
 

96285fe8cae1674991e439b5a7138274051b0d17
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-03-25 13:00:38 -0300

    Criação dos métodos da Tela de Esqueci a Senha

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from './../../models/user';
+import { UserService } from './../../services/user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
+
+export class ForgotPasswordComponent implements OnInit{
+
+  passwordRecoveryForm!: FormGroup;
+  userList!: IUser[];
+  userFound!: IUser;
+
+  constructor(private router: Router,private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsersList();
+    this.createForm();
+  }
+
+  getUsersList() {
+    this.userService.getAllUsers().subscribe((users) => {
+      this.userList = users;
+    });
+  }
+
+  createForm() {
+    this.passwordRecoveryForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(8)])
+    });
+  }
+
+  get email() {
+    return this.passwordRecoveryForm.get('email');
+  }
+
+  findEmailInUserList(): Boolean {
+    this.userFound = this.userList.find((user) => {
+      return user.email == this.email?.value;
+    })!;
+    if(this.userFound == undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  onSubmit() {
+    if(this.findEmailInUserList()) {
+      this.router.navigate(['/email-send']);
+    }else{
+      //TODO: toast with error
+    }
+  }
 
 }

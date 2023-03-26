a0c76adc0030c7d534d1d2f57671503fef92c0a2
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-03-26 18:31:56 -0300

    Criação dos métodos da Tela de Edição de Coleções

--- src/app/pages/collection/edit-collection/edit-collection.component.ts
+++ src/app/pages/collection/edit-collection/edit-collection.component.ts
@@ -1,8 +1,11 @@
+import { AlertService } from './../../../services/alert.service';
+import { IAlert } from './../../../models/alert';
 import { CollectionService } from './../../../services/collection.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ICollection } from 'src/app/models/collection';
+import { ERROR, SUCCESS } from 'src/app/common/alert-state';
 
 @Component({
   selector: 'app-edit-collection',
@@ -13,6 +16,7 @@ export class EditCollectionComponent implements OnInit {
 
   currentId: number = parseInt(this.activeRoute.snapshot.paramMap.get('id')!);
   collectionEditForm!: FormGroup;
+  alertMessage!: IAlert;
   collection: ICollection = {
     name: '',
     accountable: '',
@@ -23,13 +27,44 @@ export class EditCollectionComponent implements OnInit {
     models: 0
   }
 
-  constructor(private activeRoute: ActivatedRoute ,private router: Router, private collectionService: CollectionService) {
+  constructor(private activeRoute: ActivatedRoute ,private router: Router, private collectionService: CollectionService, private alertService: AlertService) {
 
   }
 
   ngOnInit(): void {
-    // this.getCollection();
+    this.getCollection();
     this.createForm();
+  }
+
+  getCollection() {
+    try {
+      this.collectionService.getCollection(this.currentId).subscribe((collection) => {
+        this.collection = collection;
+        this.collectionEditForm.patchValue({
+          name: collection.name,
+          accountable: collection.accountable,
+          season: collection.season,
+          brand: collection.brand,
+          budget: collection.budget,
+          release: collection.release
+        });
+      })
+
+    }catch(error) {
+      // message error
+    }
+  }
+
+  updateCollection() {
+    const collection = this.createCollectionObject();
+    try {
+      this.collectionService.updateCollection(this.currentId, collection).subscribe({
+        next: (v) => this.resultMessageCollection(v),
+        error: (e) => this.resultErrorMessageCollection(),
+      });
+    }catch(error) {
+      this.resultErrorMessageCollection();
+    }
   }
 
   createForm() {
@@ -67,11 +102,65 @@ export class EditCollectionComponent implements OnInit {
     return this.collectionEditForm.get('release');
   }
 
+  createCollectionObject(): ICollection {
+    return {
+      id: this.collection.id,
+      name: this.name?.value,
+      accountable: this.accountable?.value,
+      season: this.season?.value,
+      brand: this.brand?.value,
+      budget: this.budget?.value,
+      release: this.budget?.value,
+      models: this.collection.models,
+    }
+  }
+
   onSubmit() {
-    console.log(this.collection);
+    if(this.collectionEditForm.valid){
+      this.createCollectionObject();
+      this.updateCollection();
+    }else{
+      this.inputBlankErrorMessage();
+    }
   }
 
   cancel() {
     this.router.navigate(['/collections']);
   }
+
+  resultMessageCollection(result: any) {
+    if(result.name) {
+      this.alertMessage = {
+        title: '',
+        message: 'Coleção atualizada com sucesso!',
+        typeAlert: SUCCESS,
+      }
+      this.alertService.showGenericAlert(this.alertMessage);
+    }else {
+      this.alertMessage = {
+        title: 'Ocorreu um erro ao cadastrar a Coleção',
+        message: 'Entrar em contato com o administrador do sistema.',
+        typeAlert: ERROR,
+      }
+    }
+  }
+
+  resultErrorMessageCollection() {
+    this.alertMessage = {
+      title: 'Ocorreu um erro ao cadastrar a Coleção',
+      message: 'Entrar em contato com o administrador do sistema.',
+      typeAlert: ERROR,
+    };
+    this.alertService.showGenericAlert(this.alertMessage);
+  }
+
+  inputBlankErrorMessage() {
+    this.alertMessage = {
+      title: '',
+      message: 'Preencha os campos',
+      typeAlert: ERROR,
+    };
+    this.alertService.showGenericAlert(this.alertMessage);
+  }
+
 }

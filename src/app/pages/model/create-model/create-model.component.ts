51367df5424b67aafe6fbc59f5ba47dc234e1da4
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-03-28 08:12:28 -0300

    Criação dos métodos da Tela de Cadastro de Modelos

--- src/app/pages/model/create-model/create-model.component.ts
+++ src/app/pages/model/create-model/create-model.component.ts
@@ -1,3 +1,4 @@
+import { IModel } from './../../../models/model';
 import { IAlert } from './../../../models/alert';
 import { AlertService } from 'src/app/services/alert.service';
 import { ICollection } from 'src/app/models/collection';
@@ -5,7 +6,8 @@ import { ModelService } from './../../../services/model.service';
 import { CollectionService } from './../../../services/collection.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { ERROR } from 'src/app/common/alert-state';
+import { ERROR, SUCCESS } from 'src/app/common/alert-state';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-model',
@@ -14,9 +16,18 @@ import { ERROR } from 'src/app/common/alert-state';
 })
 export class CreateModelComponent implements OnInit {
 
+  modelCreateForm!: FormGroup;
   listOfModelTypes = ['Bermuda', 'Biquini', 'Bolsa', 'Boné', 'Calça', 'Camisa', 'Chapéu', 'Saia'];
   collections: ICollection[] = [];
   alertMessage!: IAlert;
+  model: IModel = {
+    name: '',
+    accountable: '',
+    type: '',
+    collection: '',
+    embroidery: false,
+    stamped: false,
+  }
 
   constructor(private router: Router, private collectionService: CollectionService, private modelService: ModelService, private alertService: AlertService) {
 
@@ -24,6 +35,58 @@ export class CreateModelComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCollections();
+    this.createForm();
+  }
+
+  createForm() {
+    this.modelCreateForm = new FormGroup({
+      name: new FormControl(null,[Validators.required, Validators.minLength(3)]),
+      accountable: new FormControl(null,[Validators.required, Validators.minLength(3)]),
+      type: new FormControl('',[Validators.required]),
+      collection: new FormControl('',[Validators.required]),
+      embroidery: new FormControl(null,[Validators.required]),
+      stamped: new FormControl(null,[Validators.required]),
+    });
+  }
+
+  get name() {
+    return this.modelCreateForm.get('name');
+  }
+
+  get accountable() {
+    return this.modelCreateForm.get('accountable');
+  }
+
+  get type() {
+    return this.modelCreateForm.get('type');
+  }
+
+  get collection() {
+    return this.modelCreateForm.get('collection');
+  }
+
+  get embroidery() {
+    return this.modelCreateForm.get('embroidery');
+  }
+
+  get stamped() {
+    return this.modelCreateForm.get('stamped');
+  }
+
+  createModel(): Boolean {
+    const model = this.createObjectModel();
+
+    try {
+      this.modelService.createModel(model).subscribe({
+        next: (r) => this.resultMessageModel(r),
+        error: (e) => this.resultErrorCreateModel()
+      })
+    }catch(error) {
+      this.resultErrorCreateModel();
+      return false;
+    }
+    this.modelCreateForm.reset();
+    return true;
   }
 
   getCollections() {
@@ -36,11 +99,68 @@ export class CreateModelComponent implements OnInit {
     }
   }
 
+  createObjectModel(): IModel {
+    return {
+      name: this.name?.value,
+      accountable: this.accountable?.value,
+      type: this.type?.value,
+      collection: this.collection?.value,
+      embroidery: this.embroidery?.value == 'yes',
+      stamped: this.stamped?.value == 'yes',
+    }
+  }
+
+  onSubmit() {
+    if(this.modelCreateForm.valid && (this.modelCreateForm.value.collection != '' && this.modelCreateForm.value.type != '')) {
+      this.createModel();
+    }else{
+      this.resultBlankInputsCollection();
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/models']);
+  }
+
+  resultMessageModel(result: any) {
+    if(result.name) {
+      this.alertMessage = {
+        title: '',
+        message: 'Modelo cadastrado com sucesso!',
+        typeAlert: SUCCESS,
+      }
+      this.alertService.showGenericAlert(this.alertMessage);
+    }else {
+      this.alertMessage = {
+        title: 'Ocorreu um erro ao cadastrar um Modelo',
+        message: 'Entrar em contato com o administrador do sistema.',
+        typeAlert: ERROR,
+      }
+    }
+  }
+
+  resultErrorCreateModel(): void {
+    this.alertMessage = {
+      title: 'Ocorreu um erro ao cadastrar o Modelo',
+      message: 'Entrar em contato com o administrador do sistema.',
+      typeAlert: ERROR,
+    };
+    this.alertService.showGenericAlert(this.alertMessage);
+  }
 
   resultErrorMessageColletion(): void {
     this.alertMessage = {
       title: 'Ocorreu um erro ao resgatar as Coleções',
       message: 'Entrar em contato com o administrador do sistema.',
+      typeAlert: ERROR,
+    };
+    this.alertService.showGenericAlert(this.alertMessage);
+  }
+
+  resultBlankInputsCollection() {
+    this.alertMessage = {
+      title: '',
+      message: 'Preencha os campos',
       typeAlert: ERROR,
     };
     this.alertService.showGenericAlert(this.alertMessage);
